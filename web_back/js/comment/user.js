060b4ef29e4df99c6d15fd28e2802892d08f39b5
xuhaofeng <xuhaofeng@163.com>
2019-11-20 01:17:00 +0800

    获取用户信息

--- web_back/js/comment/user.js
+++ web_back/js/comment/user.js
@@ -1,24 +1,37 @@
 var publicLocal = "http://localhost:8000/"
 var user = {
     login : function (userName,userPassworde,callback) {
-        $.post( publicLocal+'admin/login',
+        $.post( publicLocal+'admin/login',//userName
         {
             user_name: userName,
             password: userPassworde
-        },
+        },//userPassworde
         function (res) {
            callback(res)
 
         })
+        // function(res){
+        //     console.log(2)
+        //     if (res.code === 200) {
+        //         location.href = "./index.html"
+        //     } else {
+        //         $('#msgInfo').text(res.msg)
+        //         $('#myModal').modal('show')
+        //         // console.log(res.msg)
+        //     }
+        // }
     },
     logout :function (callback) {
         $.post( publicLocal+'admin/logout',function (res) {
                 callback(res)
-                // console.log(res.code);
-                
-                
+                // console.log(res.code);  
             })
         
+    },
+    getAdmin :function (callback) {
+        $.get(publicLocal+'admin/getuser',function (res){
+            callback(res)
+        })
     }
     
 }
